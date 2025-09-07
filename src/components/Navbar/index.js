0a50bdbdd66ad5e2cc339d19ef9196029e4720b4
Kaviprakash V <kaviprakashvelmurugan@gmail.com>
2025-09-07 20:24:55 +0530

    Add navbar structure and logo design

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,19 @@
+import Styles from './index.module.css'
+
+const NavBar = () =>{
+    const PVOX_LOGO  = process.env.REACT_APP_PVOX_LOGO
+    console.log('Logo URL:', PVOX_LOGO); // ← Add this line
+    return (
+        <div className={Styles.navBg}>
+            <div className={Styles.navLogo}>
+                 <img src={PVOX_LOGO} alt='pvox-logo' />
+            </div> 
+            <div className={Styles.navLogin}>
+                 <p>Explore</p>
+                 <button>Login</button>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
